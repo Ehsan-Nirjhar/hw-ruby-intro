d6564730b4cee6134ed8b93340a8793ddecdb9bf
EC2 Default User <ec2-user@ip-172-31-22-238.us-east-2.compute.internal>
2019-09-08 16:52:45 +0000

    First commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,75 @@
 
 # Part 1
 
-def sum arr
+def sum(array)
   # YOUR CODE HERE
+  val = 0
+  
+  array.each do |num|
+    val += num
+  end
+  
+  return val
 end
 
-def max_2_sum arr
+def max_2_sum(array)
   # YOUR CODE HERE
+  new_arr = array.sort.reverse
+  if new_arr.length == 0
+    return 0
+  elsif new_arr.length == 1
+    return new_arr[0]
+  else
+    return new_arr[0] + new_arr[1]
+  end
+  
 end
 
-def sum_to_n? arr, n
+def sum_to_n?(array, n)
   # YOUR CODE HERE
+  for i in 0...(array.length - 1)
+    for j in (i+1)...array.length
+      if array[i] + array[j] == n
+        return true
+      end
+    end
+  end
+  
+  return false
+  
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
+def starts_with_consonant?(s)
   # YOUR CODE HERE
+  if s =~ /\A[a-z]/i and s =~ /\A[^aeiou]/i
+    return true
+  else
+    return false
+  end
+    
 end
 
-def binary_multiple_of_4? s
+def binary_multiple_of_4?(s)
   # YOUR CODE HERE
+  if s =~ /^[0-9]*00$/ or s == '0'
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+@@isbn = 0
+@@price = 0.0
+
 end
